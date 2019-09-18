ae6c2306ada39b7f91d79fb5b98c18fbe3957418
Ramiro <ramirox95@hotmail.com>
2019-09-18 09:20:19 -0400

    correcion error video

--- js/main.js
+++ js/main.js
@@ -42,41 +42,83 @@
   }]);
 })();
 
-$('.play-button').on('click', function () {
-  $(this).hide();
-  $(this).parent().fadeOut();
-  $(this).parent().siblings('.slider-video')[0].play();
+
+//Start Youtube API
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var youtubeReady = false;
+
+//Variable for the dynamically created youtube players
+var players= new Array();
+var isPlaying = false;
+function onYouTubeIframeAPIReady(){
+  //The id of the iframe and is the same as the videoId	
+  jQuery(".youtube-video").each(function(i, obj)  {
+     players[obj.id] = new YT.Player(obj.id, {         
+			  videoId: obj.id,
+			    playerVars: {
+			    controls: 2,
+		      rel:0,
+		      autohide:1,
+		      showinfo: 0 ,
+		      modestbranding: 1,
+		      wmode: "transparent",
+		      html5: 1
+       	},    
+        events: {
+          'onStateChange': onPlayerStateChange
+        }
+       });
+     });
+     youtubeReady = true;
+  }
+
+
+function onPlayerStateChange(event) {
+  var target_control =  jQuery(event.target.getIframe()).parent().parent().parent().find(".controls");
+  
+  var target_caption = jQuery(event.target.getIframe()).parent().find(".carousel-caption");
+  switch(event.data){
+    case -1:
+      jQuery(target_control).fadeIn(500);
+      jQuery(target_control).children().unbind('click');
+      break
+     case 0:
+      jQuery(target_control).fadeIn(500);
+      jQuery(target_control).children().unbind('click');
+      break;
+     case 1:
+      jQuery(target_control).children().click(function () {return false;});
+      jQuery(target_caption).fadeOut(500);
+      jQuery(target_control).fadeOut(500);
+       break;
+      case 2:
+        jQuery(target_control).fadeIn(500);
+        jQuery(target_control).children().unbind('click'); 
+        break;
+        case 3:
+           jQuery(target_control).children().click(function () {return false;});
+           jQuery(target_caption).fadeOut(500);
+           jQuery(target_control).fadeOut(500);
+           break;
+          case 5:
+            jQuery(target_control).children().click(function () {return false;});
+            jQuery(target_caption).fadeOut(500);
+            jQuery(target_control).fadeOut(500);
+            break;
+          default:
+            break;
+    }
+};
+
+jQuery(window).bind('load', function(){
+  jQuery(".carousel-caption").fadeIn(500);
+  jQuery(".controls").fadeIn(500);
+ });
+
+jQuery('.carousel').bind('slid.bs.carousel', function (event) {
+   jQuery(".controls").fadeIn(500);
 });
-
-$('.slider-video').on('play', function () {
-  $(this).attr('controls', '1');
-});
-
-// Additionnal code for the slider
-var pos = 0,
-  slides = $('.slide'),
-  numOfSlides = slides.length;
-
-function nextSlide(){
-  stopCurrentVideo();
-  slides.eq(pos).animate({left:'-100%'},500);
-  pos = pos >= numOfSlides-1 ? 0 : ++pos;
-  slides.eq(pos).css({left:'100%'}).animate({left:0},500);
-}
-
-function previousSlide(){
-  stopCurrentVideo();
-  slides.eq(pos).animate({left:'100%'},500);
-  pos = pos == 0 ? numOfSlides-1 : --pos;
-  slides.eq(pos).css({left:'-100%'}).animate({left:0},500);
-}
-
-function stopCurrentVideo(){
-  $('.slider-video:eq('+pos+')').load().removeAttr('controls')
-  .siblings('.overlay-content').show().find('.play-button').show();
-}
-
-$('.left').click(previousSlide);
-$('.right').click(nextSlide);
-
-
